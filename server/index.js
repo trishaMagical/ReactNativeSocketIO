56577ff07ba384b74f051a435c0a2843f3f7b994
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-09-23 12:46:53 +0530

    local stage

--- server/index.js
+++ server/index.js
@@ -13,6 +13,25 @@ const io =new Server(server,{
       methods: ["GET", "POST"],
     },
   });
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (data)=>{
+        console.log(`User With ID:${socket.id} joined room: ${data}`);
+        socket.join(data)
+    });
+
+    socket.on("send_tictactoe",(data)=>{
+        console.log("send_tictactoe:",data);
+        socket.to(data.room).emit("recived_tictoc", data)
+
+    });
+
+
+    socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+      });
+})
 server.listen(port, () => {
     console.log("SERVER RUNNING:" + port);
   });
