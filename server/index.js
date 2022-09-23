c94b8fde5754a1a2e14868a0cfced43b7f106164
trishaMagical <trisha.hazra@madgicaltechdom.com>
2022-09-23 12:45:36 +0530

    Local changes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ app.use(cors());
 const port =3005;
 const server = http.createServer(app);
 
+const io =new Server(server,{
+    cors: {
+      origin: "http://localhost:19006",
+      methods: ["GET", "POST"],
+    },
+  });
 server.listen(port, () => {
     console.log("SERVER RUNNING:" + port);
   });
